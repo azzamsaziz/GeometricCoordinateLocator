a31a53fa0634cc9843705f590563259499238b75
Azzam <Azzam@DESKTOP-FOB5KG9>
2018-09-26 22:34:32 -0600

    Cleaned up the repo: - Changed default ValuesController to CoordinatesController. - Changed startup URL to the CoordinatesController GET endpoint - Removed unused namespaces.

---
+++ GeometricCoordinateLocator/Controllers/CoordinatesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GeometricCoordinateLocator.Controllers
+{
+    [Route("api/[controller]")]
+    public class CoordinatesController : Controller
+    {
+        // GET api/coordinates
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
